b3a89f296f5c7c4329a1ddd42cc47b1fbd1b6faa
mohamedcodingpro <mohaminali100@gmail.com>
2024-07-09 20:11:52 +0200

    Commit message

--- index.js
+++ index.js
@@ -11,84 +11,93 @@ function Cart() {
       smallPizzaOrder: false,
       mediumPizzaOrder: false,
       largePizzaOrder: false,
-      smallPizzaQuantity: 0,
-      mediumPizzaQuantity: 0,
-      largePizzaQuantity: 0,
+      smallPizzaCount: 0, // New property to track small pizza count
+      mediumPizzaCount: 0, // New property to track medium pizza count
+      largePizzaCount: 0, // New property to track large pizza count
       orderActive: false,
       paymentReady: false,
       thankYouText: false,
       insufficientText: false,
       payment: 0,
-      
+
       // Cart methods
       addCart(pizzaSize) {
-          // Increment pizza total based on size and update quantity
+          // Increment pizza total based on size and update count
           switch (pizzaSize) {
               case 'small':
                   this.smallPizzaTotal += this.smallPizzaPrice;
                   this.smallPizzaTotal = this.priceFormat(this.smallPizzaTotal);
-                  this.smallPizzaQuantity++;
+                  this.smallPizzaCount++;
                   break;
               case 'medium':
                   this.mediumPizzaTotal += this.mediumPizzaPrice;
                   this.mediumPizzaTotal = this.priceFormat(this.mediumPizzaTotal);
-                  this.mediumPizzaQuantity++;
+                  this.mediumPizzaCount++;
                   break;
               case 'large':
                   this.largePizzaTotal += this.largePizzaPrice;
                   this.largePizzaTotal = this.priceFormat(this.largePizzaTotal);
-                  this.largePizzaQuantity++;
+                  this.largePizzaCount++;
                   break;
           }
-          this.orderActive = true;
       },
-
       removeCart(pizzaSize) {
-          // Decrement pizza total based on size and update quantity
+          // Decrement pizza total based on size and update count
           switch (pizzaSize) {
               case 'small':
                   if (this.smallPizzaTotal > 0) {
                       this.smallPizzaTotal -= this.smallPizzaPrice;
                       this.smallPizzaTotal = this.priceFormat(this.smallPizzaTotal);
-                      this.smallPizzaQuantity--;
-                      this.smallPizzaOrder = this.smallPizzaQuantity > 0;
+                      this.smallPizzaCount--;
+                      this.smallPizzaOrder = this.smallPizzaCount > 0;
                   }
                   break;
               case 'medium':
                   if (this.mediumPizzaTotal > 0) {
                       this.mediumPizzaTotal -= this.mediumPizzaPrice;
                       this.mediumPizzaTotal = this.priceFormat(this.mediumPizzaTotal);
-                      this.mediumPizzaQuantity--;
-                      this.mediumPizzaOrder = this.mediumPizzaQuantity > 0;
+                      this.mediumPizzaCount--;
+                      this.mediumPizzaOrder = this.mediumPizzaCount > 0;
                   }
                   break;
               case 'large':
                   if (this.largePizzaTotal > 0) {
                       this.largePizzaTotal -= this.largePizzaPrice;
                       this.largePizzaTotal = this.priceFormat(this.largePizzaTotal);
-                      this.largePizzaQuantity--;
-                      this.largePizzaOrder = this.largePizzaQuantity > 0;
+                      this.largePizzaCount--;
+                      this.largePizzaOrder = this.largePizzaCount > 0;
                   }
                   break;
           }
-          // Check if any pizzas are left in the cart
-          this.orderActive = this.smallPizzaOrder || this.mediumPizzaOrder || this.largePizzaOrder;
       },
-
+      buy(pizzaSize) {
+          // Set pizza order based on size
+          switch (pizzaSize) {
+              case 'small':
+                  this.smallPizzaOrder = true;
+                  break;
+              case 'medium':
+                  this.mediumPizzaOrder = true;
+                  break;
+              case 'large':
+                  this.largePizzaOrder = true;
+                  break;
+          }
+          this.addCart(pizzaSize);
+          this.orderActive = true;
+      },
       getTotalCost() {
           // Calculate total cost and update order status
           this.totalCost = this.smallPizzaTotal + this.mediumPizzaTotal + this.largePizzaTotal;
+          this.orderActive = this.totalCost !== 0;
           return 'R' + this.totalCost.toFixed(2);
       },
-
       priceFormat(price) {
           return parseFloat(price.toFixed(2));
       },
-
       checkout() {
           this.paymentReady = true;
       },
-
       pay() {
           try {
               this.payment = parseFloat(this.payment);
@@ -118,32 +127,29 @@ function Cart() {
               }, 3000);
           }
       },
-
       reset() {
           // Reset all cart properties to their initial values
           this.totalCost = 0;
           this.smallPizzaTotal = 0;
           this.mediumPizzaTotal = 0;
           this.largePizzaTotal = 0;
+          this.smallPizzaCount = 0;
+          this.mediumPizzaCount = 0;
+          this.largePizzaCount = 0;
           this.smallPizzaOrder = false;
           this.mediumPizzaOrder = false;
           this.largePizzaOrder = false;
-          this.smallPizzaQuantity = 0;
-          this.mediumPizzaQuantity = 0;
-          this.largePizzaQuantity = 0;
           this.orderActive = false;
           this.paymentReady = false;
           this.thankYouText = false;
           this.insufficientText = false;
           this.payment = 0;
       },
-
       getChange() {
-          // Calculate change due to the customer
           this.getTotalCost();
           this.change = this.payment - this.totalCost;
           if (this.change > 0) {
-              return ' Here is your change R' + this.change.toFixed(2);
+              return 'Here is your change R' + this.change.toFixed(2);
           } else {
               return '';
           }
